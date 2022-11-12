45a30c68484a522e7d47c46fc479cc9cd15a67bb
JasonOCat <jason.catbagan@gmail.com>
2022-11-12 18:31:15 +0100

    Do 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, time) {
+    if (time < 0) {
+        return 'ERROR';
+    }
+    return str.repeat(time);
 };
 
 // Do not edit below this line
