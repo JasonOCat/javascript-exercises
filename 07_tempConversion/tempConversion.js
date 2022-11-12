30f53a0b75e06e5d4c4950cde3ed5b85fecb8fa0
JasonOCat <jason.catbagan@gmail.com>
2022-11-12 19:46:17 +0100

    Do 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
 
+  return roundTo1Decimal((fahrenheit-32) * (5/9));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return roundTo1Decimal((celsius * 9 / 5) + 32);
 };
+
+
+function roundTo1Decimal (num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
