4ff9b86e5c7ebc96a6d0c7f9520ae14f2ae9136b
JasonOCat <jason.catbagan@gmail.com>
2022-11-12 18:43:58 +0100

    Do 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arrayStr = str.split("");
+    let reverseArrayStr = arrayStr.reverse();
+    let reverseStr =reverseArrayStr.join("");
+    return reverseStr;
 };
 
 // Do not edit below this line
