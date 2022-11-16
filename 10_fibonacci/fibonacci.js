a45de781bbdc82c130bfaf63ea6d4e05c232489d
Jason Catbagan <jcatbagan@crystal-holding.fr>
2022-11-16 21:29:26 +0100

    Do 10_ fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) {
+    return 'OOPS';
+  }
+
+  if (n === 0) {
+    return 0;
+  }
+
+  let fibo_0 = 0;
+  let fibo_1 = 1;
+  let tmp;
+
+  for (let i = 2; i <= n; i++) {
+    tmp = fibo_1;
+    fibo_1 = fibo_0 + fibo_1;
+    fibo_0 = tmp; 
+  }
+
+  return fibo_1;
 
 };
 
