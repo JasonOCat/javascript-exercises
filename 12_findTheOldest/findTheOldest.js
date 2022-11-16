df09a25ad44b62effdbb5118767310997f627005
Jason Catbagan <jcatbagan@crystal-holding.fr>
2022-11-16 21:59:19 +0100

    Do 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, current) => {
 
+    let oldestAge = getAge(oldest);
+    let currentAge = getAge(current);
+
+    return oldestAge >= currentAge ? oldest : current; 
+
+  });
 };
+
+function getAge(person) {
+    if (!person.yearOfDeath) {
+        return (new Date()).getFullYear() - person.yearOfBirth;
+    }
+    else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
