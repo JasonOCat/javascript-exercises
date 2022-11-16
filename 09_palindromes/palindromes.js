f4bd66a24a8f7218bf74eab117409f4e832a6193
Jason Catbagan <jcatbagan@crystal-holding.fr>
2022-11-16 21:11:31 +0100

    Do 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let re = /[\W]/g;
+  let cleanWord = str.replace(re,''); // remove non alphabetical world;
+  cleanWord = cleanWord.toLowerCase();
 
+  return cleanWord === cleanWord.split('').reverse().join('');
 };
 
 // Do not edit below this line
